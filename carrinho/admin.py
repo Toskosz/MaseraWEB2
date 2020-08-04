b3bc7d18444abca68cea6b3f375cf4d8bcc64425
toskosz <thiago.tokarski2001@gmail.com>
2020-08-04 10:12:01 -0300

    404 html

--- carrinho/admin.py
+++ carrinho/admin.py
@@ -5,6 +5,7 @@ from .models import ItemCompra, Compra
 class ListandoCompras(admin.ModelAdmin):
     list_display = ('codigo_de_referencia', 'comprador', 'finalizada', 'data_de_compra')
     search_fields = ('comprador__cpf', 'codigo_de_referencia',)
+    filter_horizontal = ('itens',)
 
 
 class ListandoItens(admin.ModelAdmin):
