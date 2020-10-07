53a868ea0e5918e288810b5ca4ff800d3d5a5507
Toskosz <49496909+Toskosz@users.noreply.github.com>
2020-10-07 10:11:04 -0300

    Update settings.py
    secret key

--- masera/settings.py
+++ masera/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2cv0n28ltps)l(h*z2(@h*c@81^93(8(bt3m6)4gzb!4q3@-we'
+SECRET_KEY = #
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
